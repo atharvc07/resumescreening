17926ff4af1b604dcd3fd0b2ff274370ed6aaf0a
atharvc07 <atharv9jan2004@gmail.com>
2026-04-14 01:42:32 +0530

    nabling CORS and adding frontend to docker-compose

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api import routes
 from .database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="AI Resume Screening Service")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(routes.router)
 
 @app.get("/")
